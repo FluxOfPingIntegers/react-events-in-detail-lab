c3bf4773a414a79f69477405664b35851209a5c9
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-10-02 15:33:15 -0400

    Done.

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React from 'react'
+
+class DelayedButton extends React.Component {
+  handleClick = e => {
+    e.persist();
+    window.setTimeout(() => {this.props.onDelayedClick(e)}, this.props.delay)
+  }
+
+  render() {
+    return (
+    <div>
+      <button onClick={this.handleClick} >DelayedButton</button>
+    </div>
+    )
+  }
+}
+
+export default DelayedButton
